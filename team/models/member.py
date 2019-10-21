278965b5f579e199ae2f5a0cabb8de50e3849441
SHIVAM SAXENA <shivamsaxena@SHIVAMs-Home-MacBook.local>
2019-10-22 02:20:50 +0530

    adding member model

---
+++ team/models/member.py
@@ -0,0 +1,25 @@
+from django.db import models
+import uuid
+
+
+class Member(models.Model):
+
+    MEMBER_TYPE = (
+        ('admin', 'admin'),
+        ('regular', 'regular')
+    )
+
+    class Meta:
+        db_table = 'member'
+
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    email = models.CharField(max_length=100)
+    role = models.CharField(max_length=20, choices=MEMBER_TYPE)
+
+
+
+
+
