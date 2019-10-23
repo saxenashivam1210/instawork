5dc7b164e8064904eae357d71af6bb749c4620e3
SHIVAM SAXENA <shivamsaxena@SHIVAMs-Home-MacBook.local>
2019-10-23 11:21:33 +0530

    adding delete api

--- team/views/member_view.py
+++ team/views/member_view.py
@@ -2,13 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from team.models.member import Member
 from team.serializer.member import Member as MemberSerializer
-from rest_framework.parsers import JSONParser
 import json
 
 
 class MemberService(APIView):
-
-    def get(self, request):
+    @staticmethod
+    def get(request):
         requested_uuid = request.GET.get('uuid')
 
         if requested_uuid:
@@ -18,13 +17,15 @@ class MemberService(APIView):
         members = Member.objects.all()
         return Response(MemberSerializer(members, many=True).data)
 
-    def post(self, request):
+    @staticmethod
+    def post(request):
         serializer = MemberSerializer(data=json.loads(request.body))
         serializer.is_valid()
         member = serializer.save()
         return Response(MemberSerializer(member).data)
 
-    def put(self, request):
+    @staticmethod
+    def put(request):
         data = json.loads(request.body)
         uuid = data['uuid']
         existing_member = Member.objects.filter(uuid=uuid).first()
@@ -33,11 +34,11 @@ class MemberService(APIView):
         member = serializer.save()
         return Response(MemberSerializer(member).data)
 
-    def patch(self, request):
-        data = json.loads(request.body)
-        uuid = data['uuid']
-        existing_member = Member.objects.filter(uuid=uuid).first()
-        serializer = MemberSerializer(existing_member, data=data)
-        serializer.is_valid()
-        member = serializer.save()
-        return Response(MemberSerializer(member).data)
+    @staticmethod
+    def delete(request):
+        requested_uuid = request.GET.get('uuid')
+        deleted = Member.objects.filter(uuid=requested_uuid).delete()
+        if deleted[0] == 1:
+            return Response(status=200, data={"message": "Record deleted"})
+        else:
+            return Response(status=404, data={"message": "Record with uuid {} does not exist".format(requested_uuid)})
